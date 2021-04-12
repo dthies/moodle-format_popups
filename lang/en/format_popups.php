1710223725872d09a1eeed48dbbc08575887945d
Daniel Thies <dethies@gmail.com>
2021-04-11 20:07:36 -0500

    Remove unused strings

--- lang/en/format_popups.php
+++ lang/en/format_popups.php
@@ -26,19 +26,10 @@
 defined('MOODLE_INTERNAL') || die();
 
 $string['addsections'] = 'Add topics';
-$string['currentsection'] = 'This topic';
-$string['editsection'] = 'Edit topic';
-$string['editsectionname'] = 'Edit topic name';
-$string['deletesection'] = 'Delete topic';
-$string['newsectionname'] = 'New name for topic {$a}';
 $string['sectionname'] = 'Topic';
-$string['pluginname'] = 'Topics format';
 $string['section0name'] = 'General';
-$string['page-course-view-topics'] = 'Any course main page in topics format';
-$string['page-course-view-topics-x'] = 'Any course page in topics format';
 $string['hidefromothers'] = 'Hide topic';
 $string['showfromothers'] = 'Show topic';
-$string['privacy:metadata'] = 'The Topics format plugin does not store any personal data.';
 
 $string['pluginname'] = 'Pop up activities';
 $string['privacy:metadata'] = 'The Pop up activities format plugin does not store any personal data.';
