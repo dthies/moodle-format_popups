68e362fb17ad88cca357e3989b73e01b47680d94
Daniel Thies <dthies@gmail.com>
2023-01-16 17:56:44 -0600

    Add support for mod_contentdesigner

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'format_popups';
-$plugin->release = '1.0.14';
-$plugin->version = 2022040310;
+$plugin->release = '1.0.15';
+$plugin->version = 2022040311;
 $plugin->requires = 2022041900;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = [
