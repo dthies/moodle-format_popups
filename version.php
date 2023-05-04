53abf4f941787f6859c8af1a21a9a2a3894e4964
Daniel Thies <dthies@gmail.com>
2023-05-04 14:14:08 -0500

    Filter modal title

--- version.php
+++ version.php
@@ -27,6 +27,7 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'format_popups';
 $plugin->release = '1.0.15';
 $plugin->version = 2022040311;
+$plugin->incompatible = 402;
 $plugin->requires = 2022041900;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = [
