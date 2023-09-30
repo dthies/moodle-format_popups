98f38d27b0ac30e840e4750e5aab9d941c0c148e
Daniel Thies <dthies@gmail.com>
2023-09-30 11:27:00 -0500

    Handle more SCORM settings

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'format_popups';
-$plugin->release = '1.0.15';
-$plugin->version = 2023042401;
+$plugin->release = '1.0.16';
+$plugin->version = 2023042402;
 $plugin->requires = 2023042400;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = [
