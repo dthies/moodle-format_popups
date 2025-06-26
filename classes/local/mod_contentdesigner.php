efd61157b04ab35ee9dbec737766ff51e3c58cde
Daniel Thies <dthies@gmail.com>
2025-06-26 10:59:27 -0500

    Cleanup content designer changes

--- classes/local/mod_contentdesigner.php
+++ classes/local/mod_contentdesigner.php
@@ -19,26 +19,15 @@
  *
  * @package    format_popups
  * @copyright  2021 Daniel Thies <dethies@gmail.com>
- *             adapted from Moodle mod_h5pactivity
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 namespace format_popups\local;
 
-defined('MOODLE_INTERNAL') || die();
-
 use stdClass;
 use context_user;
-use core_h5p\factory;
-use core_h5p\player;
-use core_h5p\helper;
-use mod_h5pactivity\local\manager;
-use mod_h5pactivity\output\reportlink;
-use mod_h5pactivity\event\report_viewed;
 use moodle_exception;
 use moodle_url;
-
-require_once($CFG->dirroot . '/mod/h5pactivity/lib.php');
 
 /**
  * Activity renderer Popups course format
@@ -47,10 +36,8 @@ require_once($CFG->dirroot . '/mod/h5pactivity/lib.php');
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 class mod_contentdesigner extends mod_page {
-
     /** @var $contentdesigner contentdesigner instance object */
     public $contentdesigner = null;
-
 
     /**
      * Renders page contents
@@ -66,7 +53,7 @@ class mod_contentdesigner extends mod_page {
 
         $contentdesigner = $DB->get_record('contentdesigner', ['id' => $cm->instance], '*', MUST_EXIST);
 
-        $this->contentdesigner = new \mod_contentdesigner\content_display($contentdesigner,  $cm, $course, $context);
+        $this->contentdesigner = new \mod_contentdesigner\content_display($contentdesigner, $cm, $course, $context);
 
         require_capability('mod/contentdesigner:view', $context);
 
@@ -74,8 +61,7 @@ class mod_contentdesigner extends mod_page {
             throw new moodle_exception('course module is incorrect');
         }
 
-
-        if(!empty($this->data->action)) {
+        if (!empty($this->data->action)) {
             switch ($this->data->action) {
                 case 'finishattempt':
                     $this->contentdesigner->finish_attempt($this->data->attemptid, true);
